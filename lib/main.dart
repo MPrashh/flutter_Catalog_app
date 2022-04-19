2ec3b4b84a430e4658221325fc430e7d1e61b249
Prashant Mane <pramane1@publicisgroupe.net>
2022-04-19 12:27:09 +0530

    added

--- lib/main.dart
+++ lib/main.dart
@@ -1,14 +1,33 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_catalog/pages/login_page.dart';
+import 'pages/home_page.dart';
 
 void main() {
   runApp(MyApp());
 }
 
 class MyApp extends StatelessWidget {
-  const MyApp({Key? key}) : super(key: key);
+  //const MyApp({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(home: Material(child: Center(child: Text("welcome"))));
+    //int num = 30;
+
+    return MaterialApp(
+        // home: HomePage(),
+        themeMode: ThemeMode.light,
+        theme: ThemeData(primarySwatch: Colors.deepPurple),
+        darkTheme: ThemeData(brightness: Brightness.dark),
+        initialRoute: "/home",
+        routes: {
+          "/": (context) => LoginPage(),
+          "/home": (context) => HomePage(),
+          "/login": (context) => LoginPage(),
+        });
   }
 }
+ 
+
+//  String name = 'flutter';
+//  return MaterialApp(
+//         home: Material(child: Center(child: Text("welcome $name"))));
